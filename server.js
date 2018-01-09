90c18af9dcc03fe047bcd5a612c85072826e1dcc
LeviXIII <matthewthomas13@hotmail.com>
2018-01-09 16:06:05 -0500

    Change to db.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+//Taken out for deployment.
 //const config = require('./config.js');
 
 const User = require('./models/User');
