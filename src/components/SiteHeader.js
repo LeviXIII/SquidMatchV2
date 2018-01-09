b8da6243f11d444eb9d58ef449702f88ed4cdf6f
LeviXIII <matthewthomas13@hotmail.com>
2018-01-09 12:35:21 -0500

    Changed header link.

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -36,7 +36,7 @@ class SiteHeader extends Component {
   render() {
     return (
       <section className="header">
-        <h1 className="siteTitle"><Link to="/home">Squid Match</Link></h1>
+        <h1 className="siteTitle"><Link to="/choose-criteria">Squid Match</Link></h1>
         <p style={subTitle}>Find active players to play with in Splatoon 2</p>
         <Toolbar style={{ backgroundColor: 'rgba(0,0,0,0)'}}>
           {this.props.windowSize <= 640 ? (
