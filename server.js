07d36703821326f8ee70a656eaa2aea6e82c0c96
LeviXIII <matthewthomas13@hotmail.com>
2018-01-10 06:35:26 -0500

    added host name.

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const rooms = [];
 connection.on('open', () => {
   console.log('Now connected to Mongo ^_^');
   
-  const server = app.listen(PORT, () => {
+  const server = app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server now listening on port: ${PORT} =D`);
   })
 
