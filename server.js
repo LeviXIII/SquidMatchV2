9982bfb4ac613dcb8f2e731d1f40da83454aaf79
LeviXIII <matthewthomas13@hotmail.com>
2018-01-10 01:18:43 -0500

    using promises.

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const MONGO_CONNECTION_STRING = process.env.mLab;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const connection = mongoose.createConnection(MONGO_CONNECTION_STRING, { useMongoClient: true });
-//.then(() => console.log('connected to DB'))
-//.catch(err => console.log(err));
+const connection = mongoose.createConnection(MONGO_CONNECTION_STRING, { useMongoClient: true })
+.then(() => console.log('connected to DB'))
+.catch(err => console.log(err));
 
 //put this back in
 //const connection = mongoose.connection;
