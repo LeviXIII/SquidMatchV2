9be35da24d5ef68fa4379eaf5ac95426e47bd0f9
LeviXIII <matthewthomas13@hotmail.com>
2018-01-10 06:13:41 -0500

    change

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ const MONGO_CONNECTION_STRING = process.env.MONGOLAB_MAUVE_URI;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.createConnection(MONGO_CONNECTION_STRING, { useMongoClient: true })
-.then(() => console.log('connected to DB'))
-.catch(err => console.log(err));
+mongoose.createConnection(MONGO_CONNECTION_STRING, { useMongoClient: true });
+//.then(() => {})
+//.catch(err => console.log(err));
 
-const connection = mongoose.connection;
+const connection = mongoose.connection
 const secretKey = process.env.token_secretKey;
 const rooms = [];
 
