9645098e5bf5c2b3520585ee8dcf52fb676994a7
LeviXIII <matthewthomas13@hotmail.com>
2018-01-11 13:48:16 -0500

    changed build files.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const Messages = require('./models/Messages');
 const PORT = process.env.PORT || 8080;
 const MONGO_CONNECTION_STRING = process.env.MONGODB_URI;
 
-app.use(express.static("/build"));
+app.use(express.static(__dirname + "/build"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -742,6 +742,7 @@ app.put('/clear-invite', (req, res) => {
 
 //This is to ensures that the index.html file is found first.
 app.get('*', (req, res) => {
+  console.log('HERE IS YOUR PATH: ')
   console.log(__dirname + '/build/index.html');
   res.sendFile(__dirname + '/build/index.html');
 })
